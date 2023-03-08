4e22f4e5266ebc78c9f6cabf7c25a231d544a66e
Aman <73180294+palodaman@users.noreply.github.com>
2023-03-08 00:45:46 +0000

    setting jest.2

---
+++ pantrypal/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  };
