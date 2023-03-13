1800fab7ca4574497f1858f6d34dbe541c116357
julhoang <64108232+julhoang@users.noreply.github.com>
2023-03-13 09:29:33 -0700

    fixed test 1

--- pantrypal/__tests__/tests.ts
+++ pantrypal/__tests__/tests.ts
@@ -1,28 +1,29 @@
 import prisma from "../lib/prisma";
-import { it } from "node:test";
 import { createItem } from "../pages/api/createItem";
 import { expect, test } from "@jest/globals";
 import { Item } from "../lib/types";
+import { assert } from "node:console";
 
 // test add item
-test("addItem", () => {
-  it("add apple to the database", async () => {
-    // create a new item in the database
-    const item: Item = {
+test("add apple to the database", async () => {
+  // create a new item in the database
+  const item: Item = {
+    name: "pinapple-test",
+    expiry: "2021-12-31",
+    notes: "this is a test",
+    type: "fruit",
+  };
+
+  const res = await createItem(item);
+  expect(res).not.toBeNull();
+
+  const dbItem = await prisma.item.findUnique({
+    where: {
       name: "pinapple-test",
-      expiry: "2021-12-31",
-      notes: "this is a test",
-      type: "fruit",
-    };
-
-    const res = await createItem(item);
-
-    const dbItem = await prisma.item.findUnique({
-      where: {
-        name: "pinapple-test",
-      },
-    });
-
-    expect(res).toEqual(dbItem);
+    },
   });
+
+  expect(dbItem).not.toBeNull();
+
+  expect(res).toEqual(dbItem);
 });
