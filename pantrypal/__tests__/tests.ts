d786cd78998a9522af41d337e2d354dc83c507b8
chloezacharias <chloezacharias@uvic.ca>
2023-03-15 12:01:02 -0700

    Created a failing test, testing reciving multiple entries

--- pantrypal/__tests__/tests.ts
+++ pantrypal/__tests__/tests.ts
@@ -134,3 +134,23 @@ test("fetch by type from the database", async () => {
 
   expect(res).toEqual(itemExpected);
 });
+
+test("fetch multiple items from the database", async () => {
+  // fetch Apple
+    const item: Item = {
+    name: "****",
+    expiry: "****",
+    notes: "****",
+    type: "meat",
+  };
+
+  let itemExpected: { name: string, expiry: string, notes: string, type: string }[] = [
+    { "name": "eggs", "expiry": "2023-03-15", "notes": "expiring tmr", "type": "meat"},
+    { "name": "butter", "expiry": "2023-03-19", "notes": "expiring in 5 days", "type": "meat"}
+  ];
+
+  const res = await fetchItem(item);
+  expect(res).not.toBeNull();
+
+  expect(res).toEqual(itemExpected);
+});
