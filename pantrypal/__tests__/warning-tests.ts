42391621d0ceb7eacc270b6d144d373a97a4d9f5
julhoang <64108232+julhoang@users.noreply.github.com>
2023-03-15 16:13:07 -0700

    Add 2nd test

--- pantrypal/__tests__/warning-tests.ts
+++ pantrypal/__tests__/warning-tests.ts
@@ -29,3 +29,29 @@ test("expiring before today", async () => {
     },
   });
 });
+
+test("expiring before today", async () => {
+  const item: Item = {
+    name: "orange-test",
+    expiry: new Date().toLocaleDateString().slice(0, 10),
+    notes: "this is a test",
+    type: "other",
+  };
+
+  // create a new item in the database
+  const newItem = await prisma.item.create({
+    data: item,
+  });
+
+  // find all items that are expiring in 2 days
+  const res = await getExpiringItems();
+
+  expect(res).toContainEqual(item);
+
+  // delete the item from the database
+  await prisma.item.delete({
+    where: {
+      name: item.name,
+    },
+  });
+});
