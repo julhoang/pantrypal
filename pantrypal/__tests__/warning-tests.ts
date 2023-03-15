2ef493426ce69eae8aa72b1fe853cfda48e582f2
julhoang <64108232+julhoang@users.noreply.github.com>
2023-03-15 16:11:22 -0700

    Add 1st test for warning - currently failing

---
+++ pantrypal/__tests__/warning-tests.ts
@@ -0,0 +1,31 @@
+import prisma from "../lib/prisma";
+import { expect, test } from "@jest/globals";
+import { Item } from "../lib/types";
+import { getExpiringItems } from "../pages/api/warningItems";
+
+// test warning
+test("expiring before today", async () => {
+  const item: Item = {
+    name: "olives-test",
+    expiry: new Date().toLocaleDateString().slice(0, 10),
+    notes: "this is a test",
+    type: "other",
+  };
+
+  // create a new item in the database
+  const newItem = await prisma.item.create({
+    data: item,
+  });
+
+  // find all items that are expiring in 2 days
+  const res = await getExpiringItems();
+
+  expect(res).toContainEqual(item);
+
+  // delete the item from the database
+  await prisma.item.delete({
+    where: {
+      name: item.name,
+    },
+  });
+});
