463030bbb1ad3915578aaa3c9459b026dcc8205e
julhoang <64108232+julhoang@users.noreply.github.com>
2023-03-15 16:33:11 -0700

    5 tests - all passing

--- pantrypal/__tests__/warning-tests.ts
+++ pantrypal/__tests__/warning-tests.ts
@@ -3,11 +3,13 @@ import { expect, test } from "@jest/globals";
 import { Item } from "../lib/types";
 import { getExpiringItems } from "../pages/api/warningItems";
 
+const today = new Date();
+
 // test warning
-test("expiring before today", async () => {
+test("expiring today -- should throw warning", async () => {
   const item: Item = {
-    name: "olives-test",
-    expiry: new Date().toLocaleDateString().slice(0, 10),
+    name: "chicken-test",
+    expiry: today.toLocaleDateString().slice(0, 10),
     notes: "this is a test",
     type: "other",
   };
@@ -30,10 +32,14 @@ test("expiring before today", async () => {
   });
 });
 
-test("expiring before today", async () => {
+test("expiring tomorrow -- should throw warning", async () => {
+  const tomorrow = new Date(today.getTime() + 1 * 24 * 60 * 60 * 1000)
+    .toLocaleDateString()
+    .slice(0, 10);
+
   const item: Item = {
     name: "orange-test",
-    expiry: new Date().toLocaleDateString().slice(0, 10),
+    expiry: tomorrow,
     notes: "this is a test",
     type: "other",
   };
@@ -47,6 +53,96 @@ test("expiring before today", async () => {
   const res = await getExpiringItems();
 
   expect(res).toContainEqual(item);
+
+  // delete the item from the database
+  await prisma.item.delete({
+    where: {
+      name: item.name,
+    },
+  });
+});
+
+test("expiring yesterday -- should throw warning", async () => {
+  const yesterday = new Date(today.getTime() - 1 * 24 * 60 * 60 * 1000)
+    .toLocaleDateString()
+    .slice(0, 10);
+
+  const item: Item = {
+    name: "olives-test",
+    expiry: yesterday,
+    notes: "this is a test",
+    type: "other",
+  };
+
+  // create a new item in the database
+  const newItem = await prisma.item.create({
+    data: item,
+  });
+
+  // find all items that are expiring in 2 days
+  const res = await getExpiringItems();
+
+  expect(res).toContainEqual(item);
+
+  // delete the item from the database
+  await prisma.item.delete({
+    where: {
+      name: item.name,
+    },
+  });
+});
+
+test("expiring in 2 day -- should throw warning", async () => {
+  const future = new Date(today.getTime() + 2 * 24 * 60 * 60 * 1000)
+    .toLocaleDateString()
+    .slice(0, 10);
+
+  const item: Item = {
+    name: "tomato-test",
+    expiry: future,
+    notes: "this is a test",
+    type: "other",
+  };
+
+  // create a new item in the database
+  const newItem = await prisma.item.create({
+    data: item,
+  });
+
+  // find all items that are expiring in 2 days
+  const res = await getExpiringItems();
+
+  expect(res).toContainEqual(item);
+
+  // delete the item from the database
+  await prisma.item.delete({
+    where: {
+      name: item.name,
+    },
+  });
+});
+
+test("expiring in 3 day -- should not throw warning", async () => {
+  const future = new Date(today.getTime() + 3 * 24 * 60 * 60 * 1000)
+    .toLocaleDateString()
+    .slice(0, 10);
+
+  const item: Item = {
+    name: "onion-test",
+    expiry: future,
+    notes: "this is a test",
+    type: "other",
+  };
+
+  // create a new item in the database
+  const newItem = await prisma.item.create({
+    data: item,
+  });
+
+  // find all items that are expiring in 2 days
+  const res = await getExpiringItems();
+
+  expect(res).not.toContainEqual(item);
 
   // delete the item from the database
   await prisma.item.delete({
