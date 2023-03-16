dc4e5367413b02d2d244a5373ba7cd8ffbbaa34c
jenny <jluu@uvic.ca>
2023-03-16 14:50:17 -0700

    Updating apicall

--- pantrypal/pages/_app.tsx
+++ pantrypal/pages/_app.tsx
@@ -2,9 +2,21 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { callAPI } from './api/apiCall'
 
+const query = {
+  mainQuery: "Celery",
+  numIngr: 5
+}
+
+let recipes = [];
+
+function queryAPI (){
+    const response = callAPI(query);
+    // recipes = response.hits[0];
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Component {...pageProps} />
-    <button onClick={callAPI}>Make API call</button>
+    <button onClick={()=> callAPI(query)}>Make API call</button>
   </>
 }
--- pantrypal/pages/api/apiCall.ts
+++ pantrypal/pages/api/apiCall.ts
@@ -1,14 +1,32 @@
 // TODO add id and key to .env      resource: https://trekinbami.medium.com/using-environment-variables-in-react-6b0a99d83cf5
-const callAPI = async () => {
+
+//ingr= takes either MIN = 3+, a MIN-MAX = 3-5, MAX = 5
+//ingr= takes either MIN = 3%2B, a MIN-MAX = 3-5, MAX = 5
+
+// &health=sugar-conscious&health=tree-nut-free&health=vegan
+
+interface Query {
+    mainQuery: string,
+    ingr?: number,
+    health?: String[]
+}
+
+// const paramsString = "q=URLUtils.searchParams&topic=api";
+// const searchParams = new URLSearchParams(paramsString);
+
+const callAPI = async (query:Query) => {
     console.log("trying api call...");
     try {
-        const query = "celery";
+        const {mainQuery, ingr} = query;
 
         const appID = "8abfce08";
         const appKey = "c06091d57ff7df242e3138a49727e0c4";
-        const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=any&q=${query}&app_id=${appID}&app_key=${appKey}`);
+        const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=any&q=${mainQuery}&app_id=${appID}&app_key=${appKey}&ingr=3%2B`);
         const data = await res.json();
         console.log(data);
+
+        console.log(data.hits[0]);
+        // return data;
     } catch (err) {
         console.log(err);  
     }
