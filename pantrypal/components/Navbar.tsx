2f752ee8eae4c4ef01211b73d1a38665ecdd738f
julhoang <64108232+julhoang@users.noreply.github.com>
2023-04-07 23:04:44 -0700

    Update UI for front page, navbar, team page and Refractor code

---
+++ pantrypal/components/Navbar.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Box, Flex, Spacer, Heading, Button, Stack } from "@chakra-ui/react";
+import Link from "next/link";
+
+const Navbar = ({ currentPage }: { currentPage: string }) => {
+  return (
+    <Box
+      bgGradient="linear(to-r, #00ff87, #60efff)"
+      px={4}
+    >
+      <Flex
+        h={16}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Link
+          href="/"
+          style={{ textDecoration: "none" }}
+        >
+          <Heading
+            as="h1"
+            size="lg"
+          >
+            PantryPal
+          </Heading>
+        </Link>
+        <Spacer />
+
+        <Stack direction="row">
+          <Link
+            href="/pantry"
+            style={{ textDecoration: "none" }}
+          >
+            <Button
+              fontWeight={currentPage === "pantry" ? "bold" : "500"}
+              bg={currentPage === "pantry" ? "gray.100" : "transparent"}
+              _hover={{ bg: "gray.100" }}
+              size="md"
+            >
+              Pantry 🥕
+            </Button>
+          </Link>
+
+          <Link
+            href="/Recipes"
+            style={{ textDecoration: "none" }}
+          >
+            <Button
+              fontWeight={currentPage === "recipes" ? "bold" : "500"}
+              bg={currentPage === "recipes" ? "gray.100" : "transparent"}
+              _hover={{ bg: "gray.100" }}
+              size="md"
+            >
+              Recipes 🍲
+            </Button>
+          </Link>
+
+          <Link
+            href="/team"
+            style={{ textDecoration: "none" }}
+          >
+            <Button
+              size="md"
+              fontWeight={currentPage === "team" ? "bold" : "500"}
+              bg={currentPage === "team" ? "gray.100" : "transparent"}
+              _hover={{ bg: "gray.100" }}
+            >
+              Team 🤝
+            </Button>
+          </Link>
+        </Stack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Navbar;
--- pantrypal/pages/team.tsx
+++ pantrypal/pages/team.tsx
@@ -1,56 +1,25 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "@/components/Header";
+import { AbsoluteCenter, Box, Center } from "@chakra-ui/react";
+import Team from "@/components/Team";
 
-const Team = () => {
+export default function TeamPage() {
   return (
-    <Box p={4} bg="gray.100">
-      <Heading as="h2" size="lg" mb={2}>
-        Meet Our Team
-      </Heading>
-      <Text mb={4}>
-        We are a team of four passionate developers working on PantryPal. Get to know us a little better:
-      </Text>
-      <Box mb={8}>
-        <Heading as="h3" size="md" mb={2}>
-          Aman Palod
-        </Heading>
-        <Text mb={2}>V00968304</Text>
-        <Text mb={4}>
-          Aman is a 3rd year Software Engineering student at the University of Victoria. His interests include creating high-quality, user-friendly applications.
-        </Text>
-      </Box>
-      <Box mb={8}>
-        <Heading as="h3" size="md" mb={2}>
-          Chloe Zackarias
-        </Heading>
-        <Text mb={2}>V00961884</Text>
-        <Text mb={4}>
-          Chloe is a talented designer who focuses on creating beautiful, intuitive interfaces that enhance the user experience.
-        </Text>
-      </Box>
-      <Box mb={8}>
-        <Heading as="h3" size="md" mb={2}>
-          Jenny Luu
-        </Heading>
-        <Text mb={2}>V00961828</Text>
-        <Text mb={4}>
-          Jenny is an experienced backend developer who specializes in building robust, scalable systems that can handle high traffic loads.
-        </Text>
-      </Box>
-      <Box mb={8}>
-        <Heading as="h3" size="md" mb={2}>
-          Julia Hoang
-        </Heading>
-        <Text mb={2}>V00974641</Text>
-        <Text mb={4}>
-          Julia is a skilled frontend developer who is passionate about creating responsive, accessible websites that look great on any device.
-        </Text>
-      </Box>
+    <Box
+      position="relative"
+      height="100vh"
+      width={"100%"}
+      bg="gray.100"
+    >
+      <Header />
+      <Navbar currentPage={"team"} />
 
-      <Text mb={4}>
-       Special thanks to our mentors Neil Ernst and Niloofar for guiding us through out the project
-      </Text>
+      <Box>
+        <Center>
+          <Team />
+        </Center>
+      </Box>
     </Box>
   );
-};
-
-export default Team;
+}
